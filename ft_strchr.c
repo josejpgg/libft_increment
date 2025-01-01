e42334b987262fee1293b25d143d6a6095b4abe1
José Jean Pierre Gamarra Gaspar <josejp@Joses-MP.local>
2025-01-01 17:54:53 +0100

    new comment

--- ft_strchr.c
+++ ft_strchr.c
@@ -6,12 +6,16 @@
 /*   By: jgamarra <jgamarra@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/14 16:03:23 by jgamarra          #+#    #+#             */
-/*   Updated: 2024/01/14 16:03:23 by jgamarra         ###   ########.fr       */
+/*   Updated: 2025/01/01 17:54:26 by jgamarra         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+/*
+* Function that locates the first occurrence of character.
+* Character returned if found, otherwise NULL.
+*/
 char	*ft_strchr(const char *s, int c)
 {
 	while (*s != '\0')
