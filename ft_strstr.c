3c1e578722b95f4b3a2aa700ec836f72a4dc6f28
José Jean Pierre Gamarra Gaspar <josejp@Joses-MP.local>
2025-01-04 18:58:45 +0100

    remove unused variable

--- ft_strstr.c
+++ ft_strstr.c
@@ -6,7 +6,7 @@
 /*   By: jgamarra <jgamarra@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/14 16:03:23 by jgamarra          #+#    #+#             */
-/*   Updated: 2025/01/04 18:53:54 by jgamarra         ###   ########.fr       */
+/*   Updated: 2025/01/04 18:58:01 by jgamarra         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,27 +20,21 @@ char	*strstr(const char *haystack, const char *needle)
 {
 	char	*find;
 	char	*string;
-	size_t	i;
-	size_t	j;
 
 	if (*needle == '\0')
 		return ((char *)haystack);
-	i = 0;
 	while (*haystack)
 	{
 		find = (char *)needle;
 		string = (char *)haystack;
-		j = i;
 		while (*string && *string == *find)
 		{
 			string++;
 			find++;
-			j++;
 			if (*find == '\0')
 				return ((char *) haystack);
 		}
 		haystack++;
-		i++;
 	}
 	return (NULL);
 }
