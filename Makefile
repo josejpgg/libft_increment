33d097e0ea1c2a7df68d85196e4a3e6be4fceb2e
José Jean Pierre Gamarra Gaspar <josejp@Joses-MP.local>
2025-01-26 16:04:49 +0100

    function to valid if input is a space

--- Makefile
+++ Makefile
@@ -13,7 +13,7 @@ ft_exit_message.c ft_isnegnumber.c ft_isposnumber.c \
 ft_lstnew_bonus.c ft_lstadd_front_bonus.c ft_lstsize_bonus.c \
 ft_lstadd_back_bonus.c ft_lstdelone_bonus.c ft_lstclear_bonus.c \
 ft_lstiter_bonus.c ft_lstlast_bonus.c ft_lstmap_bonus.c \
-ft_strcmp.c ft_strstr.c
+ft_strcmp.c ft_strstr.c ft_isspace.c
 SRC_B = ft_lstnew_bonus.c ft_lstadd_front_bonus.c ft_lstsize_bonus.c \
 ft_lstadd_back_bonus.c ft_lstdelone_bonus.c ft_lstclear_bonus.c \
 ft_lstiter_bonus.c ft_lstlast_bonus.c ft_lstmap_bonus.c
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: jgamarra <jgamarra@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/14 16:19:24 by jgamarra          #+#    #+#             */
-/*   Updated: 2025/01/04 19:05:31 by jgamarra         ###   ########.fr       */
+/*   Updated: 2025/01/26 16:01:02 by jgamarra         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -82,5 +82,6 @@ int		ft_isposmumber(char *str);
 int		ft_isnegnumber(char *str);
 int		ft_strcmp(const char *s1, const char *s2);
 char	*ft_strstr(const char *haystack, const char *needle);
+int		ft_isspace(char c);
 
 #endif
